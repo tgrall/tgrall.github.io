ba5a69418b727dfc6f2b262a1a55c483ee9939f8
Tugdual Grall <tugdual@gmail.com>
2024-10-14 15:29:17 +0000

    first version

---
+++ src/pages/copilot-showcase.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import CopilotShowcase from '../components/CopilotShowcase';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title="Tug's Website"
+      description="Windsurf and Code. Development, DevOps with GitHub and more">
+      <main>
+        <CopilotShowcase />
+      </main>
+    </Layout>
+  );
+}
